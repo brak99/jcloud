874c6d877f67f960327bb3601e860e06cd852c38
Scott Mann <brak99@hotmail.com>
2018-02-08 10:47:00 -0700

    stats works now

---
+++ 6/main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"context"
+	"crypto/sha512"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+var idStore struct {
+	ids       []string
+	currentID int
+	lock      sync.Mutex
+}
+
+type idJob struct {
+	id       int
+	password string
+}
+
+var stats struct {
+	total            int64
+	totalRequestTime int64
+	lock             sync.Mutex
+}
+
+//StatResponse is the response returned for stats requests
+type StatResponse struct {
+	Total   int64
+	Average float64
+}
+
+var workQueue = make(chan idJob, 100)
+
+func handleShutdown(signal chan struct{}) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method == "POST" {
+			fmt.Println("Request received: %s", time.Now())
+			fmt.Println("shutting down")
+
+			signal <- struct{}{}
+			fmt.Fprintf(w, "shutdown")
+		} else {
+			http.NotFound(w, r)
+		}
+	}
+}
+
+func updateStats(nanoseconds int64) {
+	stats.lock.Lock()
+	stats.total++
+	stats.totalRequestTime += nanoseconds / int64(1000)
+	stats.lock.Unlock()
+}
+
+func handlePassword() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method == "POST" {
+			start := time.Now()
+
+			defer wg.Done()
+
+			fmt.Printf("Request received: %s\n", time.Now())
+
+			idStore.lock.Lock()
+
+			id := idStore.currentID
+			idStore.currentID++
+
+			idStore.lock.Unlock()
+
+			ret := fmt.Sprintf("%v", id)
+			fmt.Fprintf(w, ret)
+
+			password := r.URL.Query().Get("password")
+			idJobReq := idJob{id: id, password: password}
+
+			workQueue <- idJobReq
+
+			wg.Add(1)
+
+			elapsed := time.Since(start)
+
+			updateStats(elapsed.Nanoseconds())
+
+		} else {
+			http.NotFound(w, r)
+		}
+	}
+}
+
+func handleGetPassword() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method == "GET" {
+			start := time.Now()
+
+			id := strings.TrimPrefix(r.URL.Path, "/hash/")
+			fmt.Printf("%v\n", id)
+
+			idInt, _ := strconv.ParseInt(id, 10, 64)
+			fmt.Fprintf(w, idStore.ids[idInt])
+
+			elapsed := time.Since(start)
+
+			updateStats(elapsed.Nanoseconds())
+
+		} else {
+			http.NotFound(w, r)
+		}
+	}
+}
+
+func handleStatistics() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method == "GET" {
+			average := float64(stats.totalRequestTime) / float64(stats.total)
+
+			resp := StatResponse{Total: stats.total, Average: average}
+
+			js, err := json.Marshal(resp)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(js)
+
+		} else {
+			http.NotFound(w, r)
+		}
+	}
+}
+
+func initIDStore() {
+	idStore.currentID = 0
+	idStore.lock = sync.Mutex{}
+	idStore.ids = make([]string, 500)
+}
+
+func initStats() {
+	stats.total = 0
+	stats.totalRequestTime = 0
+}
+
+func storePasswordHash() {
+	select {
+	case req := <-workQueue:
+		const delay = 5000 * time.Millisecond
+
+		time.Sleep(delay)
+
+		sha512 := sha512.New()
+
+		sha512.Write([]byte(req.password))
+
+		encoded := base64.StdEncoding.EncodeToString(sha512.Sum(nil))
+
+		fmt.Printf("adding: %v\n", req.id)
+		fmt.Printf("sha512:\t\t%s\n", encoded)
+
+		idStore.ids[req.id] = encoded
+	}
+}
+
+func main() {
+
+	initIDStore()
+	initStats()
+
+	stop := make(chan struct{})
+
+	handler := handlePassword()
+	shutdown := handleShutdown(stop)
+	getPassword := handleGetPassword()
+	statistics := handleStatistics()
+
+	http.HandleFunc("/hash", handler)
+	http.HandleFunc("/shutdown", shutdown)
+	http.HandleFunc("/hash/", getPassword)
+	http.HandleFunc("/stats", statistics)
+
+	server := &http.Server{Addr: ":8088"}
+
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	go func() {
+		for {
+			storePasswordHash()
+		}
+	}()
+
+	<-stop
+	wg.Wait()
+
+	server.Shutdown(context.Background())
+}
---
+++ 6/main_test.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestMonkey(t *testing.T) {
+
+	handler := handlePassword()
+
+	r, _ := http.NewRequest("POST", "http://localhost:8088/?password=angryMonkey", nil)
+
+	w := httptest.NewRecorder()
+	handler(w, r)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
+
+}
+
+func TestMonkey_Empty(t *testing.T) {
+
+	handler := handlePassword()
+
+	r, _ := http.NewRequest("POST", "http://localhost:8088/?password=", nil)
+
+	w := httptest.NewRecorder()
+	handler(w, r)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
+
+}
+
+func TestShutdown(t *testing.T) {
+
+	stop := make(chan struct{})
+
+	handler := handleShutdown(stop)
+
+	r, _ := http.NewRequest("POST", "http://localhost:8088/shutdown?password=angryMonkey", nil)
+
+	w := httptest.NewRecorder()
+
+	go func() {
+		handler(w, r)
+
+		resp := w.Result()
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		fmt.Println(resp.StatusCode)
+		fmt.Println(resp.Header.Get("Content-Type"))
+		fmt.Println(string(body))
+
+	}()
+
+	<-stop
+
+}
+
+func TestGetPassword(t *testing.T) {
+
+	initIDStore()
+
+	idStore.currentID = 2
+	idStore.ids[0] = "something"
+	idStore.ids[1] = "bob"
+	//stop := make(chan struct{})
+
+	handler := handleGetPassword()
+
+	r, _ := http.NewRequest("GET", "http://localhost:8088/hash/1", nil)
+
+	w := httptest.NewRecorder()
+
+	go func() {
+		handler(w, r)
+
+		resp := w.Result()
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		fmt.Println(resp.StatusCode)
+		fmt.Println(resp.Header.Get("Content-Type"))
+		fmt.Println(string(body))
+		if string(body) != "bob" {
+			t.Fatal("response should be 'bob'")
+		}
+
+	}()
+
+	//<-stop
+
+}
+func TestInit(t *testing.T) {
+
+	initIDStore()
+
+	if idStore.currentID != 0 {
+		t.Fatal("currentID != 0")
+	}
+
+	if idStore.ids == nil {
+		t.Fatal("ids not initialized")
+	}
+}
+
+func TestStorePasswordHash(t *testing.T) {
+	//setup
+	initIDStore()
+
+	idJobReq := idJob{id: 0, password: "angryMonkey"}
+
+	workQueue <- idJobReq
+
+	storePasswordHash()
+
+	if idStore.ids[0] != "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q==" {
+		t.Fatal("stored password does not equal ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q==")
+	}
+}
+
+func TestStats404(t *testing.T) {
+
+	handler := handleStatistics()
+
+	r, _ := http.NewRequest("POST", "http://localhost:8088/stats", nil)
+
+	w := httptest.NewRecorder()
+	handler(w, r)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if resp.StatusCode != 404 {
+		t.Fatal("response should be 404, not %v", resp.StatusCode)
+	}
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
+
+}
+
+func TestInitStats(t *testing.T) {
+
+	initStats()
+
+	if stats.total != 0 {
+		t.Fatal("total != 0")
+	}
+
+	if stats.totalRequestTime != 0 {
+		t.Fatal("titalRequestTime != 0")
+	}
+}
+
+func TestStats(t *testing.T) {
+
+	handler := handleStatistics()
+
+	stats.total = 5
+	stats.totalRequestTime = 471
+
+	r, _ := http.NewRequest("GET", "http://localhost:8088/stats", nil)
+
+	w := httptest.NewRecorder()
+	handler(w, r)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if resp.StatusCode != 200 {
+		t.Fatal("response should be 200, not ", resp.StatusCode)
+	}
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
+
+}
+
+func TestUpdateStats(t *testing.T) {
+	stats.total = 5
+	stats.totalRequestTime = 100
+
+	updateStats(5000)
+
+	if stats.totalRequestTime != 105 {
+		t.Fatal("totalRequestTime should be 105")
+	}
+}
